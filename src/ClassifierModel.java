f58d914738721523c1377dadaba3d507a2f96b14
Nurettin Resul Tanyıldızı <tanyildiziresul@gmail.com>
2022-01-04 00:53:55 +0300

    changes

--- src/ClassifierModel.java
+++ src/ClassifierModel.java
@@ -3,42 +3,49 @@ import weka.classifiers.evaluation.ConfusionMatrix;
 import weka.classifiers.evaluation.EvaluationMetricHelper;
 import weka.classifiers.evaluation.NominalPrediction;
 import weka.classifiers.evaluation.Prediction;
-import weka.classifiers.functions.GaussianProcesses;
+import weka.classifiers.functions.*;
+import weka.classifiers.functions.supportVector.RegOptimizer;
+import weka.classifiers.functions.supportVector.RegSMO;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.classifiers.functions.LinearRegression;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Add;
 import weka.filters.unsupervised.attribute.Remove;
 
+import javax.sound.sampled.Line;
 import java.security.Guard;
 
 public class ClassifierModel {
-    public void TrainAndTestData(Instances trainData, Instances testData) {
+    public void TrainAndTestData(Instances trainData, Instances testData, boolean useFaultLine) {
         try {
             Attribute magnitude = trainData.attribute("Magnitude");
             Attribute date = trainData.attribute("EventDate");
-            //Attribute richter = trainData.attribute("RichterScale");
+            Attribute faultline = trainData.attribute("FaultLine");
 
             trainData.setClass(magnitude);
             testData.setClass(magnitude);
 
 
             Remove removeFilter = new Remove();
-            removeFilter.setAttributeIndicesArray(new int[] {date.index()});
+
+            if(!useFaultLine) {
+                removeFilter.setAttributeIndicesArray(new int[] {date.index(), faultline.index()});
+            }else    {
+                removeFilter.setAttributeIndicesArray(new int[] {date.index()});
+            }
             removeFilter.setInputFormat(trainData);
 
             Instances fTrainData = Filter.useFilter(trainData, removeFilter);
             Instances fTestData = Filter.useFilter(testData, removeFilter);
 
-            GaussianProcesses processes = new GaussianProcesses();
-            processes.buildClassifier(fTrainData);
+            LinearRegression model = new LinearRegression();
+            model.buildClassifier(fTrainData);
 
             Instances predictedData = new Instances(fTestData);
             for (int i = 0; i < testData.numInstances(); i++) {
-                double mag = processes.classifyInstance(fTestData.instance(i));
+                double mag = model.classifyInstance(fTestData.instance(i));
                 predictedData.instance(i).setValue(predictedData.attribute("Magnitude"), mag);
             }
 
--- src/EarthquakeGuesser.java
+++ src/EarthquakeGuesser.java
@@ -11,15 +11,24 @@ class EarthquakeGuesser {
 
         pp.SaveProcessedData("data/preprocessed_earthquake.arff");
 
+        EarthqInstance[] earthqInstances = new EarthqInstance[pp.data.numInstances()];
+
+        for (int i = 0; i < pp.data.numInstances(); i++) {
+            var lon = pp.data.instance(i).value(pp.data.attribute("EpicenterLon").index());
+            var lat = pp.data.instance(i).value(pp.data.attribute("EpicenterLat").index());
+            var faultLine = pp.data.instance(i).stringValue(pp.data.attribute("FaultLine").index());
+            earthqInstances[i] = new EarthqInstance(lon,lat, Integer.valueOf(faultLine));
+        }
+
+        EarthquakeMap map = new EarthquakeMap(earthqInstances);
+        map.DrawMap();
+
         TrainTestSplitter tts = new TrainTestSplitter(pp.data);
         tts.SaveTrainAndTestData();
 
-        SimpleClassifierModel simpleClassifierModel = new SimpleClassifierModel();
-        simpleClassifierModel.TrainAndTestData(tts.trainData, tts.testData);
-
         ClassifierModel classifierModel = new ClassifierModel();
-        classifierModel.TrainAndTestData(tts.trainData, tts.testData);
-
+        classifierModel.TrainAndTestData(tts.trainData, tts.testData, true);
+        classifierModel.TrainAndTestData(tts.trainData, tts.testData, false);
 
     }
 
